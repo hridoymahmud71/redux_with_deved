b24de79a3a0a50e14335cf25521e6bb9442d39a9
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-12-02 18:12:15 +0600

    Mon, Dec  2, 2019  6:12:14 PM

--- src/App.js
+++ src/App.js
@@ -1,50 +1,68 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useSelector ,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./actions";
+import { longStackSupport } from 'q';
 
 function App() {
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
   const dispatch = useDispatch();
-  const increment_by = 0;
-  const decrement_by = 0;
+  var increment_by = 0;
+  var decrement_by = 0;
 
-  onIncrementChange = (e) => {
-    var val = e.targetvalue;
-    if(!isNaN(val) ){
-      increment_by = val;
-    }
+  var increment_by_input = React.createRef();
+  var decrement_by_input = React.createRef();
+
+  function showLog() {
+    console.log(`increment by: ${increment_by}, decrement by: ${decrement_by}`);
   }
 
-  onDecrementChange = (e) => {
-    var val = e.targetvalue;
-    if(!isNaN(val) ){
-      decrement_by = val;
-    }
+
+  function getIncrementBy() {
+    return increment_by;
   }
 
-  increase = () => {
-    if(increment_by > 0){
+  function getDecrementBy() {
+    return decrement_by;
+  }
+
+  function onIncrementChange() {
+  
+    var val = increment_by_input.current.value;
+    increment_by = val;
+    increment_by_input.current.value.value = val;
+    showLog();
+  }
+
+  function onDecrementChange() {
+    var val = decrement_by_input.current.value;
+    decrement_by = val;
+    decrement_by_input.current.value.value = val;
+    showLog();
+  }
+
+  function increase() {
+    if (!isNaN(increment_by) && increment_by > 0) {
       dispatch(increment(increment_by));
-    }  
+    }
   }
 
-  decrease = () => {
-    if(increment_by > 0){
+  function decrease() {
+    if (!isNaN(decrement_by) && decrement_by > 0) {
       dispatch(decrement(decrement_by));
-    }  
+    }
   }
   return (
     <div className="App">
       <h1>Counter:{counter} </h1>
-      <button onClick={()=>this.increase()}>+</button>
-      <input id="increment_by" type="number" value={increment_by} onChange={this.onIncrementChange(e)} title="Increment by"></input>
-      <button onClick={()=>this.decrease()}>-</button>
-      <input id="decrement_by" type="number" value={increment_by}  onChange={this.onDecrementChange(e)} title="Increment by"></input>
+      <button onClick={increase}>+</button>
+      <input id="increment_by" type="number" ref={increment_by_input}  onChange={onIncrementChange} title="Increment by"></input>
+      <button onClick={decrease}>-</button>
+      <input id="decrement_by" type="number" ref={decrement_by_input}  onChange={onDecrementChange} title="Increment by"></input>
       {isLogged ? <h3>Only be seen by the logged in users</h3> : <h5>Not Logged in</h5>}
-        
+
     </div>
   );
 }
